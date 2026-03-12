feb57ce749c12c54f25702711085da767d18d85d
alfredogust <alfredog.fu@gmail.com>
2026-03-12 17:16:58 -0300

    refactor: replace plaintext passwords with bcrypt hashes

--- src/config/users.js
+++ src/config/users.js
@@ -2,13 +2,13 @@ module.exports = [
   {
     id: 1,
     email: "admin@company.com",
-    password: "admin123",
+    password: "$2b$10$mNSLeciZk51.gEUEmIFYuu666iGRsjy7X6dl3I37.briwb4PhI1Nu",
     role: "admin"
   },
   {
     id: 2,
     email: "operator@company.com",
-    password: "operator123",
+    password: "$2b$10$NnqTpFYTFNT.D9Ot4kv1SOg43DtV1ToUwSqQucFKMzzowa1WBU646",
     role: "operator"
   }
 ];
